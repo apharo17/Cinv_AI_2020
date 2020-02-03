2f69efec839936056d096811a9950b59ce15d4f1
Abner Perez <abner.perez17@gmail.com>
2020-02-02 21:01:49 -0600

    Homework 1: 1_bfs_matrix_demo1.py mult A by x
    multiplication func was programmed and tested

--- homework1/1_bfs_matrix_demo1.py
+++ homework1/1_bfs_matrix_demo1.py
@@ -138,12 +138,59 @@ def bfs(A, s, t, plot_step=False):
     return False
 
 
+def multiply(head_to_index, adj_list, vec):
+    """Performs the operation transpose(A)x, where A is represented by adj_list and x by vec"""
+    res = np.zeros(vec.shape)
+    for head in range(len(vec)):
+        if vec[head] > 0:
+            idx = head_to_index[head]
+            for tail in adj_list[idx][1:]:
+                res[tail] += 1
+    return res
+
+def mat2adjlist(A):
+    adj_list = []
+    head_to_index = {}
+    for row in range(A.shape[0]):
+        for col in range(A.shape[0]):
+            if A[row][col] > 0:
+                if row not in head_to_index:
+                    head_to_index[row] = len(adj_list)
+                    adj_list.append([row])
+                adj_list[head_to_index[row]].append(col)
+    return adj_list
+
+def bfs_matrix(n, source_to_index, adj_list, s, t):
+    explored = set()
+    frontier = []
+    frontier.append(s)
+    i = 0
+
+    while len(frontier) > 0:
+        i += 1
+
+        u = frontier.pop(0)
+        if u not in explored:
+            explored.add(u)
+
+            if u == t:
+                return True
+
+            for v in range(n):
+                if A[u][v] > 0:
+                    if v not in explored:
+                        frontier.append(v)
+
+    return False
 
 
+'''
 df = load_graph_df(["stormofswords.csv"])
 df = df[['Source', 'Target']] #Discard weights for BFS
 df.drop_duplicates(subset=['Source', 'Target'], inplace=True)
+'''
 
+'''
 #Option 1: Load from adj matrix
 node_to_index, A = get_adj_matrix(df, 'Source', 'Target')
 index_to_node = {node_to_index[node]:node for node in node_to_index.keys()}
@@ -155,17 +202,47 @@ G = nx.DiGraph()
 G.add_edges_from(edges)
 pos = nx.nx_pydot.graphviz_layout(G)
 
-#s = node_to_index['Hodor']
-#t = node_to_index['Jon']
-#print(bfs(A, s, t, plot_step=True))
-
+s = node_to_index['Hodor']
+t = node_to_index['Jon']
+print(bfs(A, s, t, plot_step=True))
+'''
 
 #Option 2: Load from adj list
+'''
 node_to_index = None
 node_to_index, adj_list = get_adj_list(df, 'Source', 'Target')
 G2 = from_adj_list(adj_list)
 pos = nx.nx_pydot.graphviz_layout(G2)
 nx.draw(G2, node_size=50, width=0.5,  arrowsize=5, pos=pos)
 plt.show()
+'''
+
+#Prueba de la funcion de multiplicacion
+A = np.zeros((7,7))
+
+A[0][1] = A[0][2] = A[0][3] = 1
+A[1][3] = A[1][4] = 1
+A[2][5] = 1
+A[3][2] = A[3][5] = A[3][6] = 1
+A[4][3] = A[4][6] = 1
+A[6][5] = 1
+print(A)
+
+adj_list = mat2adjlist(A)
+
+vec = np.zeros(7)
+vec[1] = 1
+vec[2] = 1
+vec[3] = 1
+
+i = 0
+head_to_index = {}
+for l in adj_list:
+    head = l[0]
+    head_to_index[head] = i
+    i += 1
+
+print()
+print(multiply(head_to_index, adj_list, vec))
 
 
