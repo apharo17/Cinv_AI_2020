0182e60ab8b37d73af153e68ce55ce0a99eb58fc
Abner Perez <abner.perez17@gmail.com>
2020-02-03 10:59:58 -0600

    Homework 1: 1_bfs_matrix.py version 1
    1_bfs_matrix.py version 1: matricial bfs ready, the result of the program is the evolution of the frontier. An example of a successful search: Cersei to Melisandre

--- homework1/1_bfs_matrix.py
+++ homework1/1_bfs_matrix.py
@@ -1,23 +1,93 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
-all_books = ["stormofswords.csv"]
+def load_graph_df(file_names):
+    li = []
 
-li = []
+    for f in file_names:
+        tmp = pd.read_csv(f)
+        li.append(tmp)
 
-for f in all_books:
-    tmp = pd.read_csv(f)
-    li.append(tmp)
+    df = pd.concat(li, axis=0, ignore_index=True)
+    return df
 
-df = pd.concat(li, axis=0, ignore_index=True)
 
-df = df[['Source', 'Target']]
+def get_adj_list(df, source_col, target_col):
+
+    nodes = set(df[source_col].tolist()) | set(df[target_col].tolist())
+    n = len(nodes)
+    node_to_index = {}
+    i = 0
+    for node in nodes:
+        node_to_index[node] = i
+        i += 1
+
+    source_to_index = {}
+    adj_list = []
+    for index, row in df.iterrows():
+        s = node_to_index[row['Source']]
+        t = node_to_index[row['Target']]
+        if s not in source_to_index:
+            source_to_index[s] = len(adj_list)
+            adj_list.append([s]) #Add head to the list
+        adj_list[source_to_index[s]].append(t)
+
+    return node_to_index, adj_list
+
+
+def multiply(head_to_index, adj_list, vec):
+    """Performs the operation transpose(A)x, where A is represented by adj_list and x by vec"""
+    res = np.zeros(vec.shape)
+    for head in range(len(vec)):
+        if vec[head] > 0:
+            if head in head_to_index:
+                idx = head_to_index[head]
+                for tail in adj_list[idx][1:]:
+                    res[tail] += 1
+    return res
+
+
+def bfs_matrix(n, adj_list, s, t, verbose=False):
+
+    i = 0
+    head_to_index = {}
+    for l in adj_list:
+        head = l[0]
+        head_to_index[head] = i
+        i += 1
+
+    visited = np.zeros(n)
+    visited[s] = 1
+    frontier = visited
+
+    for i in range(n):
+        #Calculate the new frontier: the neighbors of the current frontier
+        frontier = multiply(head_to_index, adj_list, frontier)
+        #Discard what has been visited
+        frontier = np.array(np.logical_and(frontier, np.logical_not(visited)), dtype='int32')
+
+        indexes = (np.nonzero(frontier))[0] #Index 0 to discard the tuple result of np.nonzero function
+
+        if verbose:
+            print(indexes)
+
+        if len(indexes) == 0:
+            break
+
+        visited[indexes] = np.ones(len(indexes))
+        if visited[t] > 0:
+            return True
+
+    return False
+
+
+df = load_graph_df(["stormofswords.csv"])
+df = df[['Source', 'Target']] #Discard weights for BFS
 df.drop_duplicates(subset=['Source', 'Target'], inplace=True)
 
-# create the networkx object
-
-G = nx.from_pandas_edgelist(df, source='Source', target='Target')
-
-nx.draw(G, node_size=50, width=0.5)
-plt.show()
+node_to_index, adj_list = get_adj_list(df, 'Source', 'Target')
+s = node_to_index['Cersei']
+t = node_to_index['Melisandre']
+print(bfs_matrix(len(node_to_index), adj_list, s, t, verbose=True))
