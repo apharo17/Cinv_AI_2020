7b8769e322f7eec7efe4b7a19d6bc561b893e144
Abner Perez <abner.perez17@gmail.com>
2020-03-18 11:57:53 -0600

    Homework Arturo 1: tarea2_demo3
    Todas las variables a float, y se elimina la variable amp

--- homework_arturo_parte1/tarea2_demo3.cu
+++ homework_arturo_parte1/tarea2_demo3.cu
@@ -8,24 +8,23 @@
 #define XMIN (-10.0)
 
 
-double f(double x) {
-	return pow(x-3,2);
+float f(float x) {
+	return pow((double)(x-3),2);
 }
 
 
 int main() {
 	
-	double x;
-	double cost;
+	float x;
+	float cost;
 	
-	double frac;
-	double temp;
-	double t;
-	double amp;
-	double delta;
-	double x_;
-	double cost_;
-	double p;
+	float frac;
+	float temp;
+	float t;
+	float delta;
+	float x_;
+	float cost_;
+	float p;
 
 	srand(time(0));
 
@@ -37,15 +36,14 @@ int main() {
 	for (int step=0; step<MAX_STEPS; step++) {
 		//printf("step: %d\t x: %.4f\t cost: %.4f\n", step, x, cost);
 
-		frac = ((double)step)/MAX_STEPS;
+		frac = ((float)step)/MAX_STEPS;
 
 		//Get temperature
 		t = (1.0-frac > 0.01) ? 1.0-frac : 0.01;
 
 		//Get random neighbour
-		amp = (XMAX-XMIN) * frac / 10.0;
 		temp = (1.0*rand()/RAND_MAX);
-    	delta = amp*temp - amp/2.0;
+    	delta = ((XMAX-XMIN)*frac/10.0)*temp - ((XMAX-XMIN)*frac/10.0)/2.0;
     	temp = (x+delta < XMAX) ? x+delta : XMAX;
     	x_ = (temp > XMIN) ? temp : XMIN;
     	
@@ -54,7 +52,7 @@ int main() {
     	if (cost_ < cost)
     		p = 1.0;
     	else
-    		p = exp(-(cost_-cost)/t);
+    		p = exp(double(-(cost_-cost)/t));
 
     	temp = (1.0*rand()/RAND_MAX);
     	if (p > temp)
@@ -63,7 +61,7 @@ int main() {
 
 	}
 
-	printf("x: %.4f\t cost: %.4f\n", x, cost);
+	printf("x: %.4f\t cost: %.4f\n", x, f(x));
 
 	return 0;
 }
