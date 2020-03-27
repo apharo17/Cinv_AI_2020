638fd69413be31ee68a3c0f5dc65a692163f5901
Abner Perez <abner.perez17@gmail.com>
2020-03-27 12:34:44 -0600

    Homework Arturo 1: tarea3_sgd_reg
    Shuffle funciona para NPOINTS igual a 64

--- homework_arturo_parte1/tarea3_sgd_reg.cu
+++ homework_arturo_parte1/tarea3_sgd_reg.cu
@@ -2,7 +2,12 @@
 #include <math.h>
 #include <time.h>
 
-#define NPOINTS 1024
+#include <stdio.h>
+#include <time.h>
+#include <curand.h>
+#include <curand_kernel.h>
+
+#define NPOINTS 64
 #define NDIMS 5
 #define ITERS 10
 
@@ -33,6 +38,42 @@ void print_points() {
 }
 
 
+__global__ void init(unsigned int seed, curandState_t *states) {
+
+  curand_init(seed, blockIdx.x, 0, &states[blockIdx.x]);
+}
+
+
+__global__ void fisher_yates(curandState_t *states, int *d_permuted) {
+  
+  int id  = threadIdx.x;
+  int temp;
+
+  extern __shared__ int perm_shared[];
+  perm_shared[2*id] = 2*id;
+  perm_shared[2*id+1] = 2*id+1;
+  __syncthreads();
+
+  unsigned int shift = 1;
+  unsigned int pos = id*2;  
+  while(shift <= blockDim.x)
+  {
+      if (curand(&states[id]) & 1) {
+        temp = perm_shared[pos];
+        perm_shared[pos] = perm_shared[pos+shift];
+        perm_shared[pos+shift] = temp;
+      }
+      
+      shift = shift << 1;
+      pos = (pos & ~shift) | ((pos & shift) >> 1);
+      __syncthreads();
+  }
+
+  d_permuted[2*id] = perm_shared[2*id];
+  d_permuted[2*id+1] = perm_shared[2*id+1];
+}
+
+
 void sgradientDescent() {
 	float eta = 0.01;
 	float w[NDIMS];
@@ -42,32 +83,47 @@ void sgradientDescent() {
 	for (int i=0; i<NDIMS; i++)
 		w[i] = 0;
 
+	curandState_t* states;
+  	cudaMalloc((void**) &states, sizeof(curandState_t)*(NPOINTS/2));
+  	init<<<(NPOINTS/2),1>>>(time(0), states);
+
+  	int indexes[NPOINTS];
+  	int index;
+  	int *d_indexes;
+  	cudaMalloc((void**) &d_indexes, sizeof(int)*NPOINTS);
+
 	
 	for (int k=0; k<ITERS; k++) {
 
+		fisher_yates<<<1,(NPOINTS/2),NPOINTS>>>(states, d_indexes);
+		cudaMemcpy(indexes, d_indexes, sizeof(int)*NPOINTS, cudaMemcpyDeviceToHost);
+
 		//Calculate the gradient of the cost
 		for (int i=0; i<NPOINTS; i++) {
+			
+			index = indexes[i];
+
 			dotprod = 0.0;
 			for (int j=0; j<NDIMS; j++) {
-				dotprod += w[j]*points_x[i][j];
+				dotprod += w[j]*points_x[index][j];
 			}
 
-			temp = 2.0*(dotprod-points_y[i]);
+			temp = 2.0*(dotprod-points_y[index]);
 			
 			for (int j=0; j<NDIMS; j++) {
-				gradient[j] = temp*points_x[i][j];
+				gradient[j] = temp*points_x[index][j];
 			}
 			for (int j=0; j<NDIMS; j++) {
 				w[j] -= eta*gradient[j];
 			}
 		}
 
-			printf("Iteration %d\n", k);
-			printf("Weights:\t");
-			for (int i=0; i<NDIMS; i++) {
-				printf("%.4f\t", w[i]);
-			}
-			printf("\n");
+		printf("Iteration %d\n", k);
+		printf("Weights:\t");
+		for (int i=0; i<NDIMS; i++) {
+			printf("%.4f\t", w[i]);
+		}
+		printf("\n");
 	}
 
 }
